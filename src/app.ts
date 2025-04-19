eed80cb2ca655dc26b7ab92b616fc7287a863ab8
Kamboji Pranav <kambojipranav@gmail.com>
2025-04-19 22:28:07 +0530

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,14 @@
 import express from 'express';
 import cors from 'cors';
 import questionRoutes from './routes/question.routes';
+import answerRoutes from './routes/answer.routes';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 app.use("/api/questions", questionRoutes);
+app.use("/api", answerRoutes); 
+
 export default app;
+
